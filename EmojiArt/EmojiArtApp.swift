cdfddb4b8c44983d2114340fd64e3a050d4e9052
Sergey Borisov <gransen@mail.ru>
2021-04-05 09:53:37 +0700

    Initial Commit

---
+++ EmojiArt/EmojiArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiArtApp.swift
+//  EmojiArt
+//
+//  Created by Sergey Borisov on 05.04.2021.
+//
+
+import SwiftUI
+
+@main
+struct EmojiArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
